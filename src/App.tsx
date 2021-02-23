eb5054193f564b8e8799e68d9d0fa6a2df572a69
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2021-02-23 15:54:30 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ExperienceBar } from './components/ExperienceBar';
 import './styles/global.css';
 
 function App() {
   return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+    
     
   );
 }
