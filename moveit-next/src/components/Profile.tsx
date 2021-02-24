2c8ca0e81bf1912a2487079ba14637b0336b3895
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2021-02-24 09:47:05 -0300

    up

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -3,11 +3,14 @@ import styles from '../styles/components/Profile.module.css';
 export function Profile() {
     return (
         <div className={styles.profileContainer}>
-            <img src="https://github.com/gabysx.png" alt="Gabys" />
-            <div>
-                <strong>Gabysx</strong>
-                <p>Level 1</p>
-            </div>
+            <img src="https://github.com/gabysx.png" alt="Gabys"/>
+           <div className={styles.strong}>
+               <strong>Gabys </strong>
+               <p>
+                   <img src="icons/level.svg" alt="Level"/>
+                   Level 1
+                </p>
+           </div>
         </div>
     );
 }
